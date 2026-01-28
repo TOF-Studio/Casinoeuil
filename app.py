9723a559cf6d18cd3c10c635956f5f21ac343473
Teamcipo <Teamcipo26@gmail.com>
2026-01-28 11:13:25 +0100

    augmentation max minebomb

--- app.py
+++ app.py
@@ -1350,7 +1350,7 @@ def minebomb_start():
     
     # ASSERTIONS
     assert bet >= 10, "Mise minimum : 10$"
-    assert bet <= 2500, "Mise maximum : 2500$ sur MineBomb"
+    assert bet <= 10000, "Mise maximum : 2500$ sur MineBomb"
     assert bet <= current_user.money, "Mise trop élevée"
     assert 3 <= bombs <= 10, "Entre 3 et 10 bombes"
     
